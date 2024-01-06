9e4aa3f97ccc2e2a20796d8fbbd396d3b5cfba65
MuBai-He <2586595521@qq.com>
2024-01-07 00:31:38 +0800

    Update Auto-Convert-Music.py

--- Auto-Convert-Music.py
+++ Auto-Convert-Music.py
@@ -2,14 +2,7 @@ import os
 import subprocess
 import requests
 import sys
-
-hd = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'
-}
-
-python_v=sys.executable+" "
-inference_base = f'inference.py'
-inference_arg = f' --vocals_only true --large_gpu --use_VOCFT true'
+from pathlib import Path
 
 
 class convert_music():
@@ -21,14 +14,24 @@ class convert_music():
         self.converting.append(name)
         self.netease_download(id,name)
 
-        self.converting.append(name)
+        #处理
+
+        self.converting.pop(0)
+        self.converted.append(name)
 
     def convert_vocals(self,song_name, vocal):
         infer_vocals_end = f'./output/{song_name}/{song_name}_vocals.wav'
-        convert_vocals = python_v + "./sovits4.1/inference_main.py " + f'-n {infer_vocals_end} -s {vocal}'
+        convert_vocals = sys.executable + " ./sovits4.1/inference_main.py " + f'-n {infer_vocals_end} -s {vocal}'
         subprocess.run(convert_vocals, shell=True)
 
+    def sep_song(self,file_path, song_name):
+        Path('./output', song_name).mkdir(parents=True, exist_ok=True)
+        inference_task = sys.executable + ' inference.py' + f' --input_audio "{file_path}" --output_folder "./output/{song_name}"' + ' --vocals_only true'
+        subprocess.run(inference_task, shell=True)
     def netease_download(self,id, name):
+        hd = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'
+        }
         if not os.path.exists('./input'):
             os.mkdir('./input')
         url = 'http://music.163.com/song/media/outer/url?id={}.mp3'
