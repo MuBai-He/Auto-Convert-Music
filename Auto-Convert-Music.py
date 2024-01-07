9e711ecbb117788bbf307b664caa5cda48bd78f1
MuBai-He <2586595521@qq.com>
2024-01-07 14:36:33 +0800

    Update Auto-Convert-Music.py

--- Auto-Convert-Music.py
+++ Auto-Convert-Music.py
@@ -4,6 +4,8 @@ import requests
 import sys
 from pathlib import Path
 from pydub import AudioSegment
+from pedalboard import Pedalboard,Compressor,NoiseGate,HighShelfFilter,Gain
+from pedalboard.io import AudioFile
 
 
 class convert_music():
@@ -16,23 +18,47 @@ class convert_music():
         self.netease_download(id=id,name=song_name)
         self.sep_song(song_name=song_name)
         self.convert_vocals(song_name=song_name,vocal=vocal)
+        self.vocal_processing(song_name=song_name, vocal=vocal)
         self.mix_music(song_name,vocal)
         self.converting.pop(0)
         self.converted.append(song_name)
-    def convert_file(self,file,song_name,vocal):
+    def convert_file(self,song_name,vocal,file):
         self.converting.append(song_name)
         self.sep_song(song_name=song_name,file_path=file)
         self.convert_vocals(song_name=song_name,vocal=vocal)
+        self.vocal_processing(song_name=song_name,vocal=vocal,file=file)
         self.mix_music(song_name,vocal)
         self.converting.pop(0)
         self.converted.append(song_name)
     def mix_music(self,song_name,vocal):
-        sound1 = AudioSegment.from_wav(rf'output/{song_name}/{song_name}_vocals_{vocal}.wav')
+        sound1 = AudioSegment.from_wav(rf'output/{song_name}/{song_name}_vocals_{vocal}_processed.wav')
         sound2 = AudioSegment.from_wav(rf'output/{song_name}/{song_name}_instrum.wav')
 
         output = sound1.overlay(sound2)  # 把sound2叠加到sound1上面
         output.export(f"output/{song_name}/{song_name}_{vocal}.wav", format="wav")  # 保存文件
 
+    def vocal_processing(self,song_name,vocal,file=""):
+        board = Pedalboard(
+            [NoiseGate(threshold_db=-15.0), Compressor(release_ms=150, attack_ms=5, threshold_db=3, ratio=3),
+             HighShelfFilter(cutoff_frequency_hz=120, gain_db=-24), HighShelfFilter(cutoff_frequency_hz=500, gain_db=3),
+             Gain(gain_db=2)])
+        if file=="":
+            vocal_path=fr"output/{song_name}/{song_name}_vocals_{vocal}.wav"
+        else:
+            vocal_path=file
+
+        with AudioFile(vocal_path) as f:
+            # Open an audio file to write to:
+            with AudioFile(fr'output/{song_name}/{song_name}_vocals_{vocal}_processed.wav', 'w', f.samplerate, f.num_channels) as o:
+                # Read one second of audio at a time, until the file is empty:
+                while f.tell() < f.frames:
+                    chunk = f.read(f.samplerate)
+
+                    # Run the audio through our pedalboard:
+                    effected = board(chunk, f.samplerate, reset=False)
+
+                    # Write the output to our output file:
+                    o.write(effected)
     def convert_vocals(self,song_name, vocal):
         infer_vocals_end = f'./output/{song_name}/{song_name}_vocals.wav'
         convert_vocals = sys.executable + " ./sovits4.1/inference_main.py " + f'-n {infer_vocals_end} -s {vocal}'
@@ -65,5 +91,5 @@ class convert_music():
 
 if __name__ =="__main__":
     c=convert_music()
-    c.convert_music_netease(id=487527980,song_name="说吧",vocal="刻晴[中]")
+    c.convert_music_netease(id=1951665069,song_name="17岁的歌",vocal="刻晴[中]")
     #c.mix_music(song_name="命运之人",vocal="刻晴[中]")
